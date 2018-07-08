4c53de20b3a74bf496fcd2441e4bdb95bbf60ad0
AllenSu <pmh621@naver.com>
2018-07-08 19:50:48 +0900

    install RSelenium via devtools

---
+++ install_RSelenium.R
@@ -0,0 +1,4 @@
+library(devtools)
+install_version("binman", version = "0.1.0", repos = "https://cran.uni-muenster.de/")
+install_version("wdman", version = "0.2.2", repos = "https://cran.uni-muenster.de/")
+install_version("RSelenium", version = "1.7.1", repos = "https://cran.uni-muenster.de/")
