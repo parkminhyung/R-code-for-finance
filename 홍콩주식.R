92afae88322ef566861d66b78f8c9fbbd6476b7e
AllenSu <pmh621@naver.com>
2018-08-15 13:26:59 +0900

    Update 홍콩주식.R

--- 홍콩주식.R
+++ 홍콩주식.R
@@ -32,8 +32,9 @@ for (i in 1:115) {
     pb$tick()
 }
 
-hk_table = do.call("rbind",hk_st) 
-hk_table = hk_table[,-c(1,4,14)]
+hk_table = do.call("rbind",hk_st) %>%
+  .[,-c(1,4,14)]
+
 hk_table[[1]] = sprintf("%05d",as.integer(hk_table[[1]]))
 View(hk_table)
 
