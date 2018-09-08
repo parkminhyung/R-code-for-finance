0db899d8b2cf4d789afa9559408f802c7c2cfd5b
AllenSu <pmh621@naver.com>
2018-09-08 23:04:21 +0900

    Update KRX_data.R

--- KRX_data.R
+++ KRX_data.R
@@ -2,65 +2,34 @@ library(rvest)
 library(quantmod)
 
 ############ creating KRX data frame ############
-KRX_list = 'http://bigdata-trader.com/itemcodehelp.jsp' %>%
+KRX_data = 'http://bigdata-trader.com/itemcodehelp.jsp' %>%
   read_html %>%
   html_nodes('table') %>%
   .[1] %>%
   html_table() %>%
   .[[1]]
-colnames(KRX_list) = c("종목코드", "종목이름","종류")  
 ######################################################
 
-############ separate KOSPI and KOSDAQ data ############
-KOSPI = data.frame()
-KOSDAQ = data.frame()
-KRX_list[grep(pattern = 'KOSDAQ',x= KRX_list[,3]),1] = paste0(KRX_list[grep(pattern = 'KOSDAQ',x= KRX_list[,3]),1],'.KQ')
-KRX_list[grep(pattern = 'KOSPI',x= KRX_list[,3]),1] = paste0(KRX_list[grep(pattern = 'KOSPI',x= KRX_list[,3]),1],'.KS')
+KRX_data = KRX_data[grep(pattern = 'KOSDAQ|KOSPI',x=KRX_data[,3]),]
+KRX_data[grep(pattern = 'KOSDAQ',x= KRX_data[,3]),1] = paste0(KRX_data[grep(pattern = 'KOSDAQ',x= KRX_data[,3]),1],'.KQ')
+KRX_data[grep(pattern = 'KOSPI',x= KRX_data[,3]),1] = paste0(KRX_data[grep(pattern = 'KOSPI',x= KRX_data[,3]),1],'.KS')
+colnames(KRX_data) = c("종목코드", "종목이름","종류")  
+View(KRX_data)
 
-KOSDAQ = KRX_list[grep(pattern = 'KOSDAQ',x= KRX_list[,3]),]
-KOSPI = KRX_list[grep(pattern = 'KOSPI',x= KRX_list[,3]),]
-
-View(rbind(KOSPI,KOSDAQ)) 
-
-############ set up ticker, date and new environ ############
-KOSPI_tickers = paste(KOSPI[,1],sep = ',',collapse = NULL)
-KOSDAQ_tickers = paste(KOSDAQ[,1],sep = ',',collapse = NULL)
-
-start_date = '2015-01-01'
+start_date = '2018-01-01'
 end_date = Sys.Date()
-KOS_data = new.env()
-KOD_data = new.env()
-############################################################
 
-########### KOSPI data ###########
-for (i in 1:length(KOSPI_tickers)) {
-  sapply(KOSPI_tickers[i], function(x){
+stock_data_kr = list()
+
+for (i in 1:nrow(KRX_data)) {
+  stock_data_kr[i] = sapply(KRX_data[i,1], function(x){
     try(
       getSymbols(
         x,
         src ="yahoo",
         from = start_date,
         to = end_date,
-        env=KOS_data,
         auto.assign = TRUE),silent = FALSE)
   })
-  cat(KOSPI_tickers[i],": download complete", "\n")
+  cat(KRX_data[i,1],": download complete", "\n")
 }
-View(KOS_data)
-
-########### KOSDAQ data ###########
-for (i in 1:length(KOSDAQ_tickers)) {
-  sapply(KOSDAQ_tickers[i], function(x){
-    try(
-      getSymbols(
-        x,
-        src ="yahoo",
-        from = start_date,
-        to = end_date,
-        env=KOD_data,
-        auto.assign = TRUE),silent = FALSE)
-  })
-  cat(KOSDAQ_tickers[i],": download complete", "\n")
-}
-View(KOD_data)
-
