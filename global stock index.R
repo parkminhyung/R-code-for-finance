4ccb4a1b6db90d88591c5324dfa363fdce1b8168
AllenSu <pmh621@naver.com>
2018-10-02 14:33:17 +0900

    Update global stock index.R

--- global stock index.R
+++ global stock index.R
@@ -1,25 +1,46 @@
 library(quantmod)
 library(rvest)
 
-global_table = 'https://in.finance.yahoo.com/world-indices/' %>%
+world_indi = 'https://finance.yahoo.com/world-indices' %>%
   read_html() %>%
-  html_nodes(xpath='//*[@id="yfin-list"]/div[2]/div/div/table') %>%
-  html_table() %>%
+  html_nodes(xpath = '//*[@id="yfin-list"]/div[2]/div/div/table') %>%
+  html_table(fill = TRUE) %>%
   .[[1]] %>%
-  .[,-c(7:9)]
+  .[,-c(6:9)] 
 
-global_table["Country"] <- NA
-global_table = global_table[,c(1:2,7,3:6)]
-global_table[3] <- c("India","India","United States","United States","Japan","Hongkong",
-                     "Australia","Taiwan","Singapore","China","Indonesia","South Korea",
-                     "United States","United States","Australia","Malaysia",
-                     "United States","United States","United States","United States","Canada",
-                     "United Kingdom","Germany","France","Eurozone","Eurozone","Eurozone",
-                     "Russia","Brazil","Mexico","Chile","Buenos Aires","Israel","Egypt","United Kingdom",
-                     "New Zealand")
-View(global_table)
+world_indi$CODE = NA 
+world_indi$COUNTRY = NA
+world_indi = world_indi[,c(1,6:7,2:5)]
+try({
+  world_indi[grep(pattern = "\\^GSPC|\\^DJI|\\^IXIC|\\^NYA|\\^XAX|\\^RUT|\\^VIX",x=world_indi[,1]),c(2:3)] = list("USA","United States")
+  world_indi[grep(pattern = '\\^BUK100P|\\^FTSE',x=world_indi[,1]),c(2:3)] = list("GBR","United Kingdom")
+  world_indi[grep(pattern = '\\^GDAXI',x=world_indi[,1]),c(2:3)] = list("DEU","GERMANY")
+  world_indi[grep(pattern = '\\^FCHI',x=world_indi[,1]),c(2:3)] = list("FRA","FRANCE")
+  world_indi[grep(pattern = '\\^STOXX50E|\\^AXJO|\\^AORD',x=world_indi[,1]),c(2:3)] = list("AUS","Australia")
+  world_indi[grep(pattern = '\\^N100',x=world_indi[,1]),c(2:3)] = list("NLD","Nederland")
+  world_indi[grep(pattern = '\\^BFX',x=world_indi[,1]),c(2:3)] = list("BEL","Belgium")
+  world_indi[grep(pattern = '\\IMOEX.ME',x=world_indi[,1]),c(2:3)] = list("RUS","Russia")
+  world_indi[grep(pattern = '\\^N225',x=world_indi[,1]),c(2:3)] = list("JPN","Japan")
+  world_indi[grep(pattern = '\\000001.SS',x=world_indi[,1]),c(2:3)] = list("CHN","China")
+  world_indi[grep(pattern = '\\^STI',x=world_indi[,1]),c(2:3)] = list("SGP","Singapore")
+  world_indi[grep(pattern = '\\^BSESN',x=world_indi[,1]),c(2:3)] = list("IND","India")
+  world_indi[grep(pattern = '\\^JKSE',x=world_indi[,1]),c(2:3)] = list("IDN","Indonesia")
+  world_indi[grep(pattern = '\\^KLSE',x=world_indi[,1]),c(2:3)] = list("MYS","Malaysia")
+  world_indi[grep(pattern = '\\^NZ50',x=world_indi[,1]),c(2:3)] = list("NZL","New Zealand")
+  world_indi[grep(pattern = '\\^KS11',x=world_indi[,1]),c(2:3)] = list("KOR","South Korea")
+  world_indi[grep(pattern = '\\^TWII',x=world_indi[,1]),c(2:3)] = list("TWN","Taiwan")
+  world_indi[grep(pattern = '\\^GSPTSE',x=world_indi[,1]),c(2:3)] = list("CAN","CANADA")
+  world_indi[grep(pattern = '\\^BVSP',x=world_indi[,1]),c(2:3)] = list("BRA","BRAZIL")
+  world_indi[grep(pattern = '\\^MXX',x=world_indi[,1]),c(2:3)] = list("MEX","MEXICO")
+  world_indi[grep(pattern = '\\^IPSA',x=world_indi[,1]),c(2:3)] = list("CHL","CHILE")
+  world_indi[grep(pattern = '\\^MERV',x=world_indi[,1]),c(2:3)] = list("ARG","Argentina")
+  world_indi[grep(pattern = '\\^TA125.TA',x=world_indi[,1]),c(2:3)] = list("ISR","Israel")
+  world_indi[grep(pattern = '\\^CASE30',x=world_indi[,1]),c(2:3)] = list("EGY","Egypt")
+  world_indi[grep(pattern = '\\^HSI',x=world_indi[,1]),c(2:3)] = list("HKG","HONGKONG")
+})
+View(world_indi)
 
-global_tickers = paste(global_table[[1]],sep = ',',collapse = NULL)
+global_tickers = paste(world_indi[[1]],sep = ',',collapse = NULL)
 start_date = '2015-01-01'
 end_date = Sys.Date()
 global_data = new.env()
@@ -35,6 +56,6 @@ for (i in 1:length(global_tickers)) {
         env=global_data,
         auto.assign = TRUE),silent = FALSE)
   })
-  cat(global_tickers[i],": download complete, Data will save into global_data", "\n")
+  cat(global_tickers[i],": download complete", "\n")
 }
 
