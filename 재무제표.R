cd5668bdcf29b43368a98ef9aeef07ac24ab0480
AllenSu <pmh621@naver.com>
2018-10-03 14:50:06 +0900

    Update 재무제표.R

--- 재무제표.R
+++ 재무제표.R
@@ -1,6 +1,6 @@
-library(rvest) #install.packages('rvest')
+library(rvest)
+library(RSelenium)
 library(plotly)
-
 
 #################get KRX stock table and create data frame ##################
 stock_table ="http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13" %>%
@@ -10,58 +10,52 @@ stock_table ="http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&sear
   .[[1]]
 
 stock_table[[2]] <- sprintf("%06d",stock_table[[2]])
+View(stock_table)
 #############################################################################
 
+pjs = wdman::phantomjs(port = 1111L, version = "2.1.1") # need to install 'wdman' package
+remDr = remoteDriver(remoteServerAddr = 'localhost',
+                     browserName ='phantomjs',
+                     port = 1111L)
+remDr$open(silent = T)
 
-#### financial statement
-fs <- function(z) {
-  com = z
-  code = grep(pattern = com,x = stock_table[,1],ignore.case = TRUE)
-  code = stock_table[code[1],2]
-  url2 = paste0('http://companyinfo.stock.naver.com/v1/company/ajax/cF1001.aspx?cmp_cd=',code,collapse = NULL)
-  txt2 = read_html(url2)
-  BS_table = txt2 %>% 
-    html_nodes("table") %>%
-    html_table(fill = TRUE) %>%
-    data.frame()
+kr_fs = function(com) {
+  ticker = stock_table[grep(pattern = com,x=stock_table[,1],ignore.case = TRUE),2] %>%
+    .[[1]]
+  remDr$navigate(paste0('https://companyinfo.stock.naver.com/v1/company/c1010001.aspx?cmp_cd=',ticker)) 
   
-  if(grepl(pattern = '해당 데이터가 존재하지 않습니다.', x=BS_table)){
-    BS_table[1,] = '데이터가 존재하지 않습니다. 기업명을 다시 확인 해 주세요'
-    BS_table = BS_table[,-c(2:9)]
-  } else {
-    rownames(BS_table) <- BS_table[,1]
-    colnames(BS_table) <- BS_table[1,] 
-    BS_table <- BS_table[-1,]
-    BS_table <- BS_table[,-1]
-  }
+  kr_table = remDr$getPageSource()[[1]] %>%
+    read_html() %>%
+    html_nodes('table') %>%
+    .[13] %>%
+    html_table(fill=TRUE) %>%
+    .[[1]]
   
-  if(grepl(pattern = '데이터가 존재하지 않습니다. 기업명을 다시 확인 해 주세요',x=BS_table)){
-    print('잘못된 데이터입니다.csv파일로 저장되지 않습니다.기업명을 다시 확인 해 주세요')
-  } else {
-    write.csv(BS_table,paste0("경로를 지정하세요",paste0(com,".csv"))) #경로지정시 'computer/document/' 이런식으로 지정,경로 끝에 /반드시 첨부
-    print('해당경로에 csv파일로 저장되었습니다')
-    View(x=BS_table,title = paste0("B/S:",com)) #단위 : 억원
-    date = gsub('\t|\r|\n','',x=colnames(BS_table)) 
-    p1 = plot_ly(data = data.frame(gsub('\t|\r|','',x=BS_table[1,])),
-                 x= ~date ,
-                 y = as.numeric(gsub(',','',BS_table[1,])),
-                 type = 'bar',name = '매출액') 
-    
-    p2 = plot_ly(data = data.frame(gsub('\t|\r','',x=BS_table[2,])),
-                 x= ~date, 
-                 y = as.numeric(gsub(',','',BS_table[2,])),
-                 type = 'bar',name = '영업이익') 
-    
-    p3 = plot_ly(data = data.frame(gsub('\t|\r','',x=BS_table[6,])),
-                 x= ~date, 
-                 y = as.numeric(gsub(',','',BS_table[6,])),
-                 type = 'bar',name = '당기순이익') 
-    subplot(p1,p2,p3,nrows = 3, shareX = TRUE)
-  }
+  colnames(kr_table) = kr_table[1,] 
+  kr_table = kr_table[-1,]
+  rownames(kr_table) = kr_table[,1]
+  kr_table = kr_table[,-1]
+  View(kr_table,title = paste0(ticker,':Finance Summary'))
+  
+  date = gsub('\t|\r|\n','',x=colnames(kr_table)) 
+  p1 = plot_ly(data = data.frame(gsub('\t|\r|','',x=kr_table[1,])),
+               x= ~date ,
+               y = as.numeric(gsub(',','',kr_table[1,])),
+               type = 'bar',name = '매출액') 
+  
+  p2 = plot_ly(data = data.frame(gsub('\t|\r','',x=kr_table[2,])),
+               x= ~date, 
+               y = as.numeric(gsub(',','',kr_table[2,])),
+               type = 'bar',name = '영업이익') 
+  
+  p3 = plot_ly(data = data.frame(gsub('\t|\r','',x=kr_table[6,])),
+               x= ~date, 
+               y = as.numeric(gsub(',','',kr_table[6,])),
+               type = 'bar',name = '당기순이익') 
+  subplot(p1,p2,p3,nrows = 3, shareX = TRUE)
 }
 
 
-#### export .csv file
+remDr$close()
+pjs$stop()
 
-
-View(stock_table)
