fbbd1f7489512e3340499b353a52ec079bb1d424
AllenSu <pmh621@naver.com>
2018-12-08 18:16:50 +0900

    Update 재무제표.R

--- 재무제표.R
+++ 재무제표.R
@@ -3,13 +3,34 @@ library(RSelenium)
 library(plotly)
 
 #################get KRX stock table and create data frame ##################
-stock_table ="http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13" %>%
+library(rvest)
+
+df_kospi ="http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=stockMkt" %>%
   read_html() %>%
   html_nodes(xpath = '/html/body/table') %>%
   html_table() %>%
   .[[1]]
+df_kospi$시장구분 = 'KOSPI'
 
-stock_table[[2]] <- sprintf("%06d",stock_table[[2]])
+df_kosdaq = "http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=kosdaqMkt" %>%
+  read_html() %>%
+  html_nodes(xpath = '/html/body/table') %>%
+  html_table() %>%
+  .[[1]]
+df_kosdaq$시장구분 = 'KOSDAQ'
+
+df_konex = "http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=konexMkt" %>%
+  read_html() %>%
+  html_nodes(xpath = '/html/body/table') %>%
+  html_table() %>%
+  .[[1]]
+df_konex$시장구분 = 'KONEX'
+
+stock_table = rbind(df_kospi,df_kosdaq,df_konex) %>%
+  sprintf("%06d",.[[2]]) 
+
+stock_table[[2]] = sprintf("%06d",stock_table[[2]]) 
+stock_table = stock_table[,c(1,2,10,3:9)]
 View(stock_table)
 #############################################################################
 
