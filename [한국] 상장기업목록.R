be582f9df9e68aa9be899851bf8d8c4aea925c3b
AllenSu <pmh621@naver.com>
2020-08-29 18:42:21 +0900

    Update [한국] 상장기업목록.R

--- [한국] 상장기업목록.R
+++ [한국] 상장기업목록.R
@@ -1,69 +1,66 @@
-library(rvest)
-
-krx_stock_table = function(market=NULL,save_csv_file = FALSE){
+krx_stock_table = function (market = NULL,save_csv_file = FALSE){
+  
+  library(rvest)
+  list  = c("konexMkt","kosdaqMkt","stockMkt")
+  url = "http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType="
   
   if(is.null(market)){
-    df_kospi ="http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=stockMkt" %>%
-      read_html() %>%
-      html_nodes(xpath = '/html/body/table') %>%
-      html_table() %>%
-      .[[1]]
-    df_kospi$시장구분 = 'KOSPI'
+    dfkl = data.frame()
+    for (i in 1:length(list)) {
+      dfk = paste0(url,list[i]) %>%
+        read_html() %>%
+        html_nodes(xpath = "/html/body/table") %>%
+        html_table() %>%
+        .[[1]]
+      
+      if(i==1) dfk$"시장구분" = "KONEX"
+      if(i==2) dfk$"시장구분" = "KOSDAQ"
+      if(i==3) dfk$"시장구분" = "KOSPI"
+      dfkl = rbind(dfk,dfkl)
+    }
+    dfkl[2] = sprintf("%06d",dfkl[[2]])
+    dfkl = dfkl[,c(1,2,10,3:9)]
+    View(dfkl,title = "상장목록")
     
-    df_kosdaq = "http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=kosdaqMkt" %>%
+  } else if(market == "kospi"){
+    dfkl = paste0(url,list[3]) %>%
       read_html() %>%
-      html_nodes(xpath = '/html/body/table') %>%
+      html_nodes(xpath = "/html/body/table") %>%
       html_table() %>%
       .[[1]]
-    df_kosdaq$시장구분 = 'KOSDAQ'
+    dfkl$"시장구분" = "KOSPI"
+    dfkl[2] = sprintf("%06d",dfkl[[2]])
+    dfkl = dfkl[,c(1,2,10,3:9)]
+    View(dfkl,title = paste0(toupper(market),"_상장목록"))
     
-    df_konex = "http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=konexMkt" %>%
+  } else if(market == "kosdaq"){
+    dfkl = paste0(url,list[2]) %>%
       read_html() %>%
-      html_nodes(xpath = '/html/body/table') %>%
+      html_nodes(xpath = "/html/body/table") %>%
       html_table() %>%
       .[[1]]
-    df_konex$시장구분 = 'KONEX'
+    dfkl$"시장구분" = "KOSDAQ"
+    dfkl[2] = sprintf("%06d",dfkl[[2]])
+    dfkl = dfkl[,c(1,2,10,3:9)]
+    View(dfkl,title = paste0(toupper(market),"_상장목록"))
     
-    stock_table = rbind(df_kospi,df_kosdaq,df_konex)
-    stock_table[[2]] = sprintf("%06d",stock_table[[2]]) 
-    stock_table = stock_table[,c(1,2,10,3:9)]
-    View(stock_table)
-  } else if(market == 'kospi') {
-    df_kospi ="http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=stockMkt" %>%
+  } else if(market =="konex"){
+    dfkl = paste0(url,list[1]) %>%
       read_html() %>%
-      html_nodes(xpath = '/html/body/table') %>%
+      html_nodes(xpath = "/html/body/table") %>%
       html_table() %>%
       .[[1]]
-    df_kospi$시장구분 = 'KOSPI'
-    df_kospi[[2]] = sprintf("%06d",df_kospi[[2]]) 
-    stock_table = df_kospi[,c(1,2,10,3:9)]
-    View(stock_table)
-  } else if(market=='kosdaq'){
-    df_kosdaq = "http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=kosdaqMkt" %>%
-      read_html() %>%
-      html_nodes(xpath = '/html/body/table') %>%
-      html_table() %>%
-      .[[1]]
-    df_kosdaq$시장구분 = 'KOSDAQ'
-    df_kosdaq[[2]] = sprintf("%06d",df_kosdaq[[2]]) 
-    stock_table = df_kosdaq[,c(1,2,10,3:9)]
-    View(stock_table)
-  } else if(market == 'konex'){
-    df_konex = "http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13&marketType=konexMkt" %>%
-      read_html() %>%
-      html_nodes(xpath = '/html/body/table') %>%
-      html_table() %>%
-      .[[1]]
-    df_konex$시장구분 = 'KONEX'
-    df_konex[[2]] = sprintf("%06d",df_konex[[2]]) 
-    stock_table = df_konex[,c(1,2,10,3:9)]
-    View(stock_table)
+    dfkl$"시장구분" = "KONEX"
+    dfkl[2] = sprintf("%06d",dfkl[[2]])
+    dfkl = dfkl[,c(1,2,10,3:9)]
+    View(dfkl,title = paste0(toupper(market),"_상장목록"))
+    
   } else {
-    print("입력이 잘못되었습니다. 다시 입력해주시기 바랍니다.")
+    print("market 값을 잘못 입력하셨습니다.")
   }
   
   if(save_csv_file == TRUE) {
-    write.csv(stock_table,paste0("KRX_",market,"_테이블.csv"))
+    write.csv(dfkl,paste0("KRX_",market,"_테이블.csv"))
     print(paste0("csv파일로 저장되었습니다.파일명 :KRX_",market,"_테이블.csv"))
   } else {
     print("csv파일로 저장하지 않고 바로 테이블을 띄웁니다.")
