2fc85f875f2e4ccd0a20898bfce80f47b96ac41c
AllenSu <pmh621@naver.com>
2021-02-05 01:28:12 +0900

    Update 블랙숄즈모형.R

--- 블랙숄즈모형.R
+++ 블랙숄즈모형.R
@@ -1,7 +1,7 @@
 black_scholes = function(s,x,rf,std,t,y=NULL,D=NULL,r=NULL){
   if(!is.null(y) & is.null(D)){
     ### dividend rate is y    
-    d1 = (log(s/x)+(rf-y+std/2)*t)/(std*sqrt(t)) 
+    d1 = (log(s/x)+(rf-y+std^2/2)*t)/(std*sqrt(t)) 
     d2 = d1-(std*sqrt(t)) 
     call.price = s*exp(-y*t)*pnorm(d1)-x*exp(-rf*t)*pnorm(d2)
     put.price = x*exp(-rf*t)*pnorm(-d2) - s*exp(-y*t)*pnorm(-d1)
@@ -11,14 +11,14 @@ black_scholes = function(s,x,rf,std,t,y=NULL,D=NULL,r=NULL){
     k = ifelse(is.null(r),1,(1+r)^t)    
     s.d = s - D/k
     
-    d1 = (log(s.d/x)+(rf+std/2)*t)/(std*sqrt(t)) 
+    d1 = (log(s.d/x)+(rf+std^2/2)*t)/(std*sqrt(t)) 
     d2 = d1-(std*sqrt(t)) 
     call.price = s.d*pnorm(d1)-x*exp(-rf*t)*pnorm(d2)
     put.price = x*exp(-rf*t)*pnorm(-d2) - s.d*pnorm(-d1)
     
   } else {
     ### no dividends
-    d1 = (log(s/x)+(rf+std/2)*t)/(std*sqrt(t)) 
+    d1 = (log(s/x)+(rf+std^2/2)*t)/(std*sqrt(t)) 
     d2 = d1-(std*sqrt(t)) 
     call.price = s*pnorm(d1)-x*exp(-rf*t)*pnorm(d2)
     put.price = x*exp(-rf*t)*pnorm(-d2) - s*pnorm(-d1)
