500646b480ca41b3e28ed46bb5f45f21587e033f
ALLEN PARK <pmh621@naver.com>
2022-01-20 19:22:42 +0900

    Update 네이버 주가 데이터 .R

--- 네이버 주가 데이터 .R
+++ 네이버 주가 데이터 .R
@@ -1,82 +1,38 @@
-library(rvest)
-ticker = "005930"
 
-##########################################################################################
-################################### price per minute info ################################
 
-{
-  '%ni%' = Negate('%in%')
+fin_data = function(ticker){
+  if(!require(rvest)) install.packages('rvest'); library(rvest)
+  if(!require(qdapRegex)) install.packages('qdapRegex'); library(qdapRegex)
+  if(!require(lubridate)) install.packages('lubridate'); library(lubridate)
   
-  if(weekdays(Sys.Date()) %ni% c("Saturday", "Sunday","Monday") & ("00:00:00" < format(Sys.time(), "%X")) & (format(Sys.time(), "%X") < "09:00:00")==TRUE) {
-    Date.time = format(Sys.Date()-1,"%Y%m%d")
-  } else if(weekdays(Sys.Date())=="Saturday"){
-    Date.time = format(Sys.Date()-1,"%Y%m%d")
-  } else if(weekdays(Sys.Date())=="Sunday"){
-    Date.time = format(Sys.Date()-2,"%Y%m%d")
-  } else if(weekdays(Sys.Date()) == "Monday" & ("00:00:00" < format(Sys.time(), "%X")) & (format(Sys.time(), "%X") < "09:00:00") == TRUE){
-    Date.time = format(Sys.Date()-3,"%Y%m%d")
-  } else {
-    Date.time = format(Sys.Date(),"%Y%m%d")
-  }
-  
+  df1 = data.frame()
   df = data.frame()
-  
-  num = paste0('https://finance.naver.com/item/sise_time.nhn?code=',ticker,'&thistime=',Date.time,'1600&page=',1) %>%
-    read_html(encoding = 'EUC-KR') %>%
-    html_nodes(xpath = '/html/body/table[2]') %>%
-    as.character() %>%
-    substr(.,gregexpr("맨뒤",.)[[1]][1]-4,gregexpr("맨뒤",.)[[1]][1]-3)
-  
-  for (i in 1:num) {
-    url = paste0('https://finance.naver.com/item/sise_time.nhn?code=',ticker,'&thistime=',Date.time,'1600&page=',i) %>%
-      read_html(encoding = 'EUC-KR') %>%
-      html_nodes(xpath = '/html/body/table[1]') %>%
-      html_table() %>%
-      .[[1]]
-    df = rbind(df,url)
-  }
-  
-  df = df[-which(df[,1]==""),] %>%
-    .[nrow(.):1,]
-  rownames(df) = df[,1] 
-  df = df[,-1] 
-  
-  for(i in 1:ncol(df)) df[,i] = gsub(',','',x=df[,i]) %>%  as.numeric()
-  
-  View(df)
-  
-}
-
-##########################################################################################
-################################### Daily price info ####################################
-
-{
-  df = data.frame()
-  
-  for (i in 1:10) {
-    url = paste0('https://finance.naver.com/item/sise_day.nhn?code=',ticker,'&page=',i) %>%
-      read_html() %>%
-      html_nodes(xpath = '/html/body/table[1]') %>%
-      html_table() %>%
-      .[[1]] 
+  for (i in 1:12) {
     
-    df = rbind(df,url)
+    ind = paste0('https://m.stock.naver.com/api/item/getPriceDayList.nhn?code=',ticker,'&pageSize=20&page=',i)%>%
+      read_html() %>%
+      html_nodes('body') %>%
+      html_text(trim = TRUE) %>%
+      rm_between(., "[", "]", extract=TRUE) %>%
+      rm_between(., "{", "}", extract=TRUE) %>% .[[1]] %>%
+      strsplit(x=.,split = ",") 
+    
+    for (i in 1:length(ind)) {
+      df1[i,1] = parse_number(ind[[i]])[1]
+      df1[i,1] = parse_number(ind[[i]])[6]
+      df1[i,2] = parse_number(ind[[i]])[7]
+      df1[i,3] = parse_number(ind[[i]])[8]
+      df1[i,4] = parse_number(ind[[i]])[2]
+      df1[i,5] = parse_number(ind[[i]])[9]
+      df1[i,6] = round(parse_number(ind[[i]])[5],digits=2) %>% paste0(.,"%")
+      rownames(df1)[i] = ymd(parse_number(ind[[i]][1])) %>% as.character()
+    }
+    df1 = df1[nrow(df1):1,]
+    df = rbind(df1,df) 
   }
   
-  df = df[-which(df[,1]==""),] %>%
-    .[nrow(.):1,]
-  
-  df[,1] = gsub("\\.","/",df[,1])
-  rownames(df) = df[,1]
-  
-  df = df[,-1] %>%
-    .[,c(3:5,1,6,2)]
-  
-  for(i in 1:5) df[,i] = gsub(',','',x=df[,i]) %>%  as.numeric()
-  
-  df[1,6] = NA
-  for (i in 2:nrow(df)) df[i,6] = paste0(round(((df[i,4]-df[i-1,4])/df[i-1,4])*100,digits=2),"%") 
-  
+  colnames(df) = c("Open","High","Low","Close","Vol","Chg")
   View(df)
+  df
 }
 
