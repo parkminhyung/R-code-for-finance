e2def00dfdbf07039ada69fbfe89596b1c64b964
ALLEN PARK <pmh621@naver.com>
2022-03-06 13:59:32 +0900

    Add files via upload

---
+++ 주식베타.R
@@ -0,0 +1,54 @@
+#BETA
+
+#############################################################
+
+beta = function(ind,mkt){
+  library(plotly)
+  "%+%" = paste0
+  options(scipen = 100)
+  IND.RET = round(diff(ind)/ind,digits=5)
+  MKT.RET = round(diff(mkt)/mkt,digits=5)
+  BETA = round(cov(IND.RET,MKT.RET)/var(MKT.RET),digits = 3)
+}
+
+
+##############################################################
+
+beta = function(ind,mkt){
+  library(plotly)
+  "%+%" = paste0
+  options(scipen = 100)
+  IND.RET = round(diff(ind)/ind,digits=5)
+  MKT.RET = round(diff(mkt)/mkt,digits=5)
+  BETA = round(cov(IND.RET,MKT.RET)/var(MKT.RET),digits = 3)
+  
+  model = lm(MKT.RET~IND.RET)
+  len = length(IND.RET)
+  
+  x_range = seq(min(IND.RET), max(IND.RET), length.out = len) 
+  slope = model$coefficients[2]
+  
+  plot_ly(
+    x=IND.RET,
+    y=MKT.RET,
+    type = "scatter",
+    mode = "markers",
+    name = "Market-Stock"
+  ) %>%
+    add_trace(
+      x=x_range,
+      y=slope*x_range,
+      mode = "lines",alpha = 1,
+      name = "linear reg") %>%
+    layout(title = "BETA: " %+% BETA) %>%
+    show()
+  cat("==========================================","\n")
+  cat(" BETA is " %+% BETA,"\n")
+  cat("y= " %+% round(slope,digits = 4) %+% "*x " %+% round(model$coefficients[1],digits=4),"\n")
+  cat("Adj.R-Squared: " %+% round(summary(model)$adj.r.squared,digits = 4),"\n")
+  cat("==========================================","\n")
+}
+
+
+
+
