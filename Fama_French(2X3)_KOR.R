2ca04eb61baa63aed4765382b49d3de9bdecfcd5
ALLEN PARK <pmh621@naver.com>
2023-05-25 17:28:05 +0900

    Update Fama_French(2X3)_KOR.R

--- Fama_French(2X3)_KOR.R
+++ Fama_French(2X3)_KOR.R
@@ -6,16 +6,14 @@ pacman::p_load("dplyr","lubridate","tibble","readxl","plotly")
 urls = c('http://www.fnindex.co.kr/factordetail/excel/3FM.2B3.B',
          'http://www.fnindex.co.kr/factordetail/excel/3FM.2B3.S',
          'http://www.fnindex.co.kr/factordetail/excel/3FM.2B3.X')
-filenames = c("FFmodel(2X3)B.xlsx",
-             "FFmodel(2X3)S.xlsx",
-             "FFmodel(2X3)X.xlsx")
-for (i in 1:length(urls)) {
-  download.file(urls[i],
-                destfile = filenames[i])
-}
+filelist = c("FFmodel(2X3)B.xlsx",
+              "FFmodel(2X3)S.xlsx",
+              "FFmodel(2X3)X.xlsx")
+
+for (i in 1:length(urls)) download.file(urls[i],destfile = filelist[i])
 
 #load files and preprocess data
-filelist = filenames %>%
+filelist = filelist %>%
   sapply(.,function(x) {
     read_excel(x) %>%
       as.data.frame(.) %>%
@@ -32,23 +30,15 @@ for (i in 2:length(filelist)) FF_model_2x3 = merge(FF_model_2x3, filelist[[i]], 
 FF_model_2x3 = FF_model_2x3 %>% 
   column_to_rownames("일자")
 
-#visualize
-FF_model_2x3 %>%
-  plot_ly(
-    x = rownames(.),
-    y = ~HIGH_B ,
-    type = 'scatter',
-    mode = 'line',
-    name = "HIGH_B"
-  ) %>%
-  add_lines(y = ~HIGH_S,name = "HIGH_S") %>%
-  add_lines(y = ~MEDIUM_B,name = "MEDIUM_B") %>%
-  add_lines(y = ~MEDIUM_S,name = "MEDIUM_S") %>%
-  add_lines(y = ~LOW_B,name = "LOW_B") %>%
-  add_lines(y = ~LOW_S,name = "LOW_S") %>%
-  add_lines(y = ~HML,name = "HML") %>%
-  add_lines(y = ~SMB,name = "SMB") %>%
+pig = plot_ly()
+for (i in 1:ncol(FF_model_2x3)) {
+  pig = pig %>% 
+    add_lines(data = FF_model_2x3,
+              x = rownames(FF_model_2x3),
+              y = FF_model_2x3[,i], 
+              name = names(FF_model_2x3)[i])
+}
+pig %>% 
   layout(title = "<b> Fama-French Model (2x3) </b>",
          margin = list(t=50,b=100),
          xaxis = list(title = "Date"), yaxis = list(title = "Value"))
-
