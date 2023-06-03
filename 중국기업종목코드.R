e983674cfc2f34086c1bdfa396448e11f89ee0a6
ALLEN PARK <pmh621@naver.com>
2023-06-03 16:41:46 +0900

    Update 중국기업종목코드.R

--- 중국기업종목코드.R
+++ 중국기업종목코드.R
@@ -1,26 +1,26 @@
-#在上证A,深证A,科创板,京证,创业板上市企业的列表(含)
+ #在上证A,深证A,科创板,京证,创业板上市企业的列表(含)
 #提供企业名,现价,交易量等的数据
 #상해,심천 등 상장회사 정보 제공
 
 pacman::p_load("rvest","dplyr")
 
-market = c("hs","ss","bj","cyb","kcb")
+markets = c("hs","ss","bj","cyb","kcb")
 '%+%' = paste0
 
 df = data.frame()
 View(df)
 
-for (i in 1:length(market)) {
+for (market in markets) {
   
-  len = 'http://q.10jqka.com.cn/index/index/board/' %+% market[i] %+% '/field/zdf/order/desc/page/1'  %>%
+  len = 'http://q.10jqka.com.cn/index/index/board/' %+% market %+% '/field/zdf/order/desc/page/1'  %>%
     read_html() %>%
     html_nodes(xpath = '//*[@id="m-page"]/span') %>%
     html_text(trim = TRUE) %>%
     gsub(x=.,pattern = "1/","")
   
-  for (j in 1:len) {
+  for (i in 1:len) {
     
-    df = 'http://q.10jqka.com.cn/index/index/board/' %+% market[i] %+% '/field/zdf/order/desc/page/' %+% j  %>%
+    df = 'http://q.10jqka.com.cn/index/index/board/' %+% market %+% '/field/zdf/order/desc/page/' %+% i  %>%
       read_html() %>%
       html_table() %>% .[[1]] %>%
       `colnames<-`(c("x","종목코드","회사명","현재주가","등락률","등락","x",
@@ -28,11 +28,11 @@ for (i in 1:length(market)) {
       select(-x) %>% 
       mutate(종목코드 = sprintf("%06d", 종목코드)) %>%
       mutate(등락률 = 등락률 %+% "%") %>%
-      mutate(시장 = ifelse(market[i] == "hs","상해A",
-                         ifelse(market[i] == "ss","심천A",
-                                ifelse(market[i] == "bj","북경A",
-                                       ifelse(market[i] == "cyb","창업판",
-                                              ifelse(market[i] == "kcb","과창판",NA)))))) %>%
+      mutate(시장 = ifelse(market == "hs","상해A",
+                         ifelse(market == "ss","심천A",
+                                ifelse(market == "bj","북경A",
+                                       ifelse(market == "cyb","창업판",
+                                              ifelse(market == "kcb","과창판",NA)))))) %>%
       rbind(.,df)
   }
   
