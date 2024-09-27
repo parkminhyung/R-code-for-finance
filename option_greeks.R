de46221b6ca79dc3a6489aa58400039b679e2bfe
ALLEN PARK <pmh621@naver.com>
2024-09-27 17:02:28 +0900

    Update option_greeks.R

--- option_greeks.R
+++ option_greeks.R
@@ -1,9 +1,5 @@
-#Sensitivity Analysis of Option Premium
-
-library(stats)
-
-option_greeks <- function(s, k, rf, sigma, tau, y = 0) {
-  tau <- tau / 365
+option_greeks <- function(s, k, rf, sigma, tau, y) {
+  tau <- tau / 252
   sigma <- sigma / 100
   rf <- rf / 100
   y <- y / 100
@@ -14,80 +10,31 @@ option_greeks <- function(s, k, rf, sigma, tau, y = 0) {
   nd1 <- (1 / (sqrt(2 * pi))) * exp(-(d1^2 / 2))
 
   # delta
-  call.Delta <- pnorm(d1)*exp(-y*tau)
-  put.Delta <- (pnorm(d1) - 1)*exp(-y*tau)
+  call_delta <- pnorm(d1) * exp(-y * tau)
+  put_delta <- (pnorm(d1) - 1) * exp(-y * tau)
 
   # Gamma
-  Gam <- (nd1*exp(-y*tau)) / (s * sigma * sqrt(tau))
+  gamma <- (nd1 * exp(-y * tau)) / (s * sigma * sqrt(tau))
 
   # theta
-  call.Th <- (-((s * sigma*exp(-y*tau)) / (2 * sqrt(tau))) * nd1 - k * exp(-rf * tau) * rf * pnorm(d2)) * (1 / 365)
-  put.Th <- (-((s * sigma*exp(-y*tau)) / (2 * sqrt(tau))) * nd1 - k * exp(-rf * tau) * rf * (pnorm(d2) - 1)) * (1 / 365)
+  call_theta <- (-((s * sigma * exp(-y * tau)) / (2 * sqrt(tau))) * nd1 - k * exp(-rf * tau) * rf * pnorm(d2)) * (1 / 252)
+  put_theta <- (-((s * sigma * exp(-y * tau)) / (2 * sqrt(tau))) * nd1 - k * exp(-rf * tau) * rf * (pnorm(d2) - 1)) * (1 / 252)
 
   # rho
-  call.rh <- k * tau * exp(-rf * tau) * pnorm(d2) * (1 / 100)
-  put.rh <- k * tau * exp(-rf * tau) * (pnorm(d2) - 1) * (1 / 100)
+  call_rho <- k * tau * exp(-rf * tau) * pnorm(d2) * (1 / 100)
+  put_rho <- k * tau * exp(-rf * tau) * (pnorm(d2) - 1) * (1 / 100)
 
   # Vega
-  Vega <- s * sqrt(tau) * nd1 * (1 / 100)
+  vega <- s * sqrt(tau) * nd1 * (1 / 100)
 
-  cat("=========== Sensitivity Analysis of Option ===========", "\n")
-  cat("\n")
-  cat("##### Delta #####", "\n")
-  cat("Call Delta : ", call.Delta, "\n")
-  cat("Put Delta :", put.Delta, "\n")
-  cat("\n")
-  cat("##### Gamma #####", "\n")
-  cat("Call/Put Gamma :", Gam, "\n")
-  cat("\n")
-  cat("##### Vega #####", "\n")
-  cat("Call/Put Vega :", Vega, "\n")
-  cat("\n")
-  cat("##### Theta #####", "\n")
-  cat("Call Theta :", call.Th, "\n")
-  cat("Put Theta :", put.Th, "\n")
-  cat("\n")
-  cat("##### Rho #####", "\n")
-  cat("Call Rho :", call.rh, "\n")
-  cat("Put Rho :", put.rh, "\n")
-  cat("\n")
-  cat("===============================================", "\n")
+  return(list(
+    call.Delta = call_delta,
+    put.Delta = put_delta,
+    Gamma = gamma,
+    Vega = vega,
+    call.Theta = call_theta,
+    put.Theta = put_theta,
+    call.Rho = call_rho,
+    put.Rho = put_rho
+  ))
 }
-
-
-# Example
-
-s = 222.66 #underlying price
-k = 222.5 #strike price
-tau = 9 #days
-sigma = 24.99 #percent
-rf  = 4.12 #risk-free rate (percent)
-y= 0.044 #dividend yield (percent)
-
-option_greeks(s,k,rf,sigma,tau)
-
-
-# Outcomes: 
-# =========== Sensitivity Analysis of Option =========== 
-
-# ##### Delta ##### 
-# Call Delta :  0.5254462 
-# Put Delta : -0.4745538 
-
-# ##### Gamma ##### 
-# Call/Put Gamma : 0.04556613 
-
-# ##### Vega ##### 
-# Call/Put Vega : 0.139201 
-
-# ##### Theta ##### 
-# Call Theta : -0.2060483 
-# Put Theta : -0.1809587 
-
-# ##### Rho ##### 
-# Call Rho : 0.02794118 
-# Put Rho : -0.02686612 
-
-# =============================================== 
-
-
